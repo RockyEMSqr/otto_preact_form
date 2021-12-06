5b6abdd5b0d659b5770d8e2502f1cf6840fff4be
rocky <rocco.cutri@emsquared-inc.com>
2021-12-06 13:30:43 -0500

    more tests

--- test/my-test-app/src/main.tsx
+++ test/my-test-app/src/main.tsx
@@ -1,5 +1,6 @@
 import { render } from 'preact'
 import { App } from './app'
 import './index.css'
-
+import {hello} from 'otto_preact_form';
+hello();
 render(<App />, document.getElementById('app')!)
