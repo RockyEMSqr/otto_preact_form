e5339f142450f6af3902ebfd797901195d7df684
rocky <rocco.cutri@emsquared-inc.com>
2022-02-01 13:39:50 -0500

    build

--- dist/src/form/input.d.ts
+++ dist/src/form/input.d.ts
@@ -4,37 +4,17 @@ export declare type Item = {
     name: any;
     value: any;
 };
-declare type Join<K, P> = K extends string | number ? P extends string | number ? `${K}${"" extends P ? "" : "."}${P}` : never : never;
-declare type Prev = [
-    never,
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    ...0[]
-];
-declare type Leaves<T, D extends number = 10> = [D] extends [never] ? never : T extends object ? {
-    [K in keyof T]-?: Join<K, Leaves<T[K], Prev[D]>>;
-}[keyof T] : "";
-export declare type InputNameCheckProps<T> = {
-    name?: T extends object ? Leaves<T> : string;
+declare type NotRecursiveKeyOf<ObjectType extends object> = {
+    [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object ? {
+        [Key1 in keyof ObjectType[Key] & (string | number)]: ObjectType[Key][Key1] extends object ? `${Key}.${Key1}` | {
+            [Key2 in keyof ObjectType[Key][Key1] & (string | number)]: ObjectType[Key][Key1][Key2] extends object ? `${Key}.${Key1}.${Key2}` | {
+                [Key3 in keyof ObjectType[Key][Key1][Key2] & (string | number)]: ObjectType[Key][Key1][Key2] extends object ? `${Key}.${Key1}.${Key2}.${Key3}` | 'Max Hit. Tell Rocky if you need more' : `${Key}.${Key1}.${Key2}.${Key3}`;
+            }[keyof ObjectType[Key][Key1][Key2] & (string | number)] : `${Key}.${Key1}.${Key2}`;
+        }[keyof ObjectType[Key][Key1] & (string | number)] : `${Key}.${Key1}`;
+    }[keyof ObjectType[Key] & (string | number)] : `${Key}`;
+}[keyof ObjectType & (string | number)];
+export declare type InputNameCheckProps<T = void> = {
+    name?: T extends void ? string : T extends Object ? NotRecursiveKeyOf<T> : string;
 };
 export declare type InputProps = {
     linkTo?: {
