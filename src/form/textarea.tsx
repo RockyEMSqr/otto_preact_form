ed5592c5728d58360919be470bc198b7453cac8a
rocky <rocco.cutri@emsquared-inc.com>
2022-03-01 00:30:56 -0500

    bare naked text area

--- src/form/textarea.tsx
+++ src/form/textarea.tsx
@@ -1,24 +1,8 @@
 import { Input, InputNameCheckProps } from "./input";
 import { h } from 'preact'; // error using vite?
 export class Textarea<T> extends Input<InputNameCheckProps<T>>{
-
-    shouldComponentUpdate(nextProps: any, nextState: any, nextContext: any) {
-        //todo(rc): why this always return true? Whats did it fix? what browser?
-        return true;
-    };
     te: HTMLTextAreaElement | undefined | null;
     type = "";
-
-    componentDidMount() {
-        if (this.te) {
-            this.te.value = this.getValue();
-        }
-    }
-    componentWillReceiveProps() {
-        if (this.te) {
-            this.te.value = this.getValue();
-        }
-    }
 
     getValue() {
         let val = super.getValue();
@@ -32,7 +16,7 @@ export class Textarea<T> extends Input<InputNameCheckProps<T>>{
         let cols = props.cols || null;
         return <div class="form-group vertical-top">
             <label for={this.props.name} class={this.labelClass}>{this.props.label}</label>
-            <textarea disabled={props.disabled} ref={x => this.te = x} autocomplete="off" onInput={this.getOnChange()} required={this.props.required} name={this.props.name} onChange={this.getOnChange()} class={this.inputClass} rows={rows} cols={cols}></textarea>
+            <textarea disabled={props.disabled} value={this.getValue()} ref={x => this.te = x} autocomplete="off" onInput={this.getOnChange()} required={this.props.required} name={this.props.name} onChange={this.getOnChange()} class={this.inputClass} rows={rows} cols={cols}></textarea>
         </div>
     }
 }
