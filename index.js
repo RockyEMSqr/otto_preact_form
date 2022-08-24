1c74e31254a48e0e6b1ed258a1704780d17f98ba
rocky <rocco.cutri@emsquared-inc.com>
2022-08-24 10:53:55 -0600

    test1

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+exports.__esModule = true;
+__exportStar(require("./src/form/address"), exports);
+__exportStar(require("./src/form/checkbox"), exports);
+__exportStar(require("./src/form/checkboxGroup"), exports);
+__exportStar(require("./src/form/choosy"), exports);
+__exportStar(require("./src/form/email"), exports);
+__exportStar(require("./src/form/file"), exports);
+__exportStar(require("./src/form/form"), exports);
+__exportStar(require("./src/form/hidden"), exports);
+__exportStar(require("./src/form/input"), exports);
+__exportStar(require("./src/form/number"), exports);
+__exportStar(require("./src/form/password"), exports);
+__exportStar(require("./src/form/phone"), exports);
+__exportStar(require("./src/form/radio"), exports);
+__exportStar(require("./src/form/select"), exports);
+__exportStar(require("./src/form/text"), exports);
+__exportStar(require("./src/form/textarea"), exports);
+__exportStar(require("./src/form/datetime"), exports);
+__exportStar(require("./src/form/richTextEditor"), exports);
+__exportStar(require("./src/components/loadingComponent"), exports);
+__exportStar(require("./src/components/modal"), exports);
+__exportStar(require("./src/components/page"), exports);
+__exportStar(require("./src/components/tabs"), exports);
+__exportStar(require("./src/components/typeAhead"), exports);
+__exportStar(require("./src/hooks"), exports);
--- index.ts
+++ index.ts
@@ -1,28 +1,30 @@
-export * from './src/form/address';
-export * from './src/form/checkbox';
-export * from './src/form/checkboxGroup';
-export * from './src/form/choosy';
-export * from './src/form/email';
-export * from './src/form/file';
-export * from './src/form/form';
-export * from './src/form/hidden';
-export * from './src/form/input';
-export * from './src/form/number';
-export * from './src/form/password';
-export * from './src/form/phone';
-export * from './src/form/radio';
-export * from './src/form/select';
-export * from './src/form/text';
-export * from './src/form/textarea';
-export * from './src/form/datetime';
-export * from './src/form/richTextEditor';
+// export * from './src/form/address';
+// export * from './src/form/checkbox';
+// export * from './src/form/checkboxGroup';
+// export * from './src/form/choosy';
+// export * from './src/form/email';
+// export * from './src/form/file';
+// export * from './src/form/form';
+// export * from './src/form/hidden';
+// export * from './src/form/input';
+// export * from './src/form/number';
+// export * from './src/form/password';
+// export * from './src/form/phone';
+// export * from './src/form/radio';
+// export * from './src/form/select';
+// export * from './src/form/text';
+// export * from './src/form/textarea';
+// export * from './src/form/datetime';
+// export * from './src/form/richTextEditor';
 
 
-export * from './src/components/loadingComponent'
-export * from './src/components/modal'
-export * from './src/components/page';
-export * from './src/components/tabs';
-export * from './src/components/typeAhead';
+// export * from './src/components/loadingComponent'
+// export * from './src/components/modal'
+// export * from './src/components/page';
+// export * from './src/components/tabs';
+// export * from './src/components/typeAhead';
 
 
-export * from './src/hooks';
+// export * from './src/hooks';
+
+export {Text} from './src/form/text';
