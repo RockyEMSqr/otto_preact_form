6ca0d575404f761f433510112298f7e9633ccf50
rocky <rocco.cutri@emsquared-inc.com>
2022-08-26 11:03:41 -0600

    dist

--- dist/src/form/form.d.ts
+++ dist/src/form/form.d.ts
@@ -1,5 +1,4 @@
 import { h, Component } from 'preact';
-import 'formdata-polyfill';
 export declare class Form extends Component<any, any> {
     formData(form: HTMLFormElement): FormData;
     formDataToJSON(form: HTMLFormElement): {};
